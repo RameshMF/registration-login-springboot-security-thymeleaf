8a3063c6cec7e475de4a674b1c411cbe1c930bfd
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-04-24 15:09:02 +0300

    Many fixes in code.

--- src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
+++ src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
@@ -1,12 +1,9 @@
 package com.example.registrationlogindemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
